bcdc6302273fb41f77500265d99b0d2b8638ee4f
Mia Pan <miapanyuewei@gmail.com>
2019-10-22 06:30:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,8 +81,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto]={},
-programmer_hash[[:yukihiro_matsumoto][known_for]="Ruby",
+programmer_hash[:yukihiro_matsumoto]={}
+programmer_hash[:yukihiro_matsumoto][known_for]="Ruby"
+programmer_hash[:dennis_ritchie][:languages]="LISP", "C"
     
 end
 
